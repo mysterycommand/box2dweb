6da95895f4bc9d6a2d3f6d4ce8096c54fbb532da
Matt Hayes <matt@mysterycommand.com>
2018-11-05 11:08:33 -0500

    refactor(demo/example): use the new modular stuff in demo and example

--- src/demo.js
+++ src/demo.js
@@ -1,56 +1,79 @@
-import { Box2D } from './box2d';
+import { Box2D } from './box2dweb';
+
+const scale = 1 / 29.075;
+
+const {
+  Common: {
+    Math: { b2Vec2: Vec2 },
+  },
+  Collision: { b2AABB: Aabb },
+  Dynamics: { b2BodyDef: BodyDef },
+  Dynamics: { b2Body: Body },
+  Dynamics: { b2FixtureDef: FixtureDef },
+  Dynamics: { b2Fixture: Fixture },
+  Dynamics: { b2World: World },
+  Collision: {
+    Shapes: { b2MassData: MassData },
+  },
+  Collision: {
+    Shapes: { b2PolygonShape: PolygonShape },
+  },
+  Collision: {
+    Shapes: { b2CircleShape: CircleShape },
+  },
+  Dynamics: { b2DebugDraw: DebugDraw },
+  Dynamics: {
+    Joints: { b2MouseJointDef: MouseJointDef },
+  },
+} = Box2D;
+
+const c = document.getElementById('canvas');
+const ctx = c.getContext('2d');
 
 function init() {
-  var b2Vec2 = Box2D.Common.Math.b2Vec2,
-    b2AABB = Box2D.Collision.b2AABB,
-    b2BodyDef = Box2D.Dynamics.b2BodyDef,
-    b2Body = Box2D.Dynamics.b2Body,
-    b2FixtureDef = Box2D.Dynamics.b2FixtureDef,
-    b2Fixture = Box2D.Dynamics.b2Fixture,
-    b2World = Box2D.Dynamics.b2World,
-    b2MassData = Box2D.Collision.Shapes.b2MassData,
-    b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape,
-    b2CircleShape = Box2D.Collision.Shapes.b2CircleShape,
-    b2DebugDraw = Box2D.Dynamics.b2DebugDraw,
-    b2MouseJointDef = Box2D.Dynamics.Joints.b2MouseJointDef;
-
-  var world = new b2World(
-    new b2Vec2(0, 10), //gravity
+  var world = new World(
+    new Vec2(0, 30), //gravity
     true, //allow sleep
   );
 
-  var fixDef = new b2FixtureDef();
+  var fixDef = new FixtureDef();
   fixDef.density = 1.0;
   fixDef.friction = 0.5;
   fixDef.restitution = 0.2;
 
-  var bodyDef = new b2BodyDef();
+  var bodyDef = new BodyDef();
 
   // create ground
-  bodyDef.type = b2Body.b2_staticBody;
-  fixDef.shape = new b2PolygonShape();
-  fixDef.shape.SetAsBox(20, 2);
-  bodyDef.position.Set(10, 400 / 30 + 1.8);
+  bodyDef.type = Body.b2_staticBody;
+  fixDef.shape = new PolygonShape();
+
+  fixDef.shape.SetAsBox(c.width * scale, 1);
+
+  bodyDef.position.Set(0, c.height * scale);
   world.CreateBody(bodyDef).CreateFixture(fixDef);
-  bodyDef.position.Set(10, -1.8);
+
+  bodyDef.position.Set(0, -1);
   world.CreateBody(bodyDef).CreateFixture(fixDef);
-  fixDef.shape.SetAsBox(2, 14);
-  bodyDef.position.Set(-1.8, 13);
+
+  fixDef.shape.SetAsBox(1, c.height * scale);
+
+  bodyDef.position.Set(-1, 0);
   world.CreateBody(bodyDef).CreateFixture(fixDef);
-  bodyDef.position.Set(21.8, 13);
+
+  bodyDef.position.Set(c.width * scale, 0);
   world.CreateBody(bodyDef).CreateFixture(fixDef);
 
   // create some objects
-  bodyDef.type = b2Body.b2_dynamicBody;
+  bodyDef.type = Body.b2_dynamicBody;
   for (var i = 0; i < 10; ++i) {
     if (Math.random() > 0.5) {
-      fixDef.shape = new b2PolygonShape();
+      fixDef.shape = new PolygonShape();
       fixDef.shape.SetAsBox(
         Math.random() + 0.1, //half width
         Math.random() + 0.1, //half height
       );
     } else {
-      fixDef.shape = new b2CircleShape(
+      fixDef.shape = new CircleShape(
         Math.random() + 0.1, //radius
       );
     }
@@ -60,12 +83,12 @@ function init() {
   }
 
   // setup debug draw
-  var debugDraw = new b2DebugDraw();
-  debugDraw.SetSprite(document.getElementById('canvas').getContext('2d'));
+  var debugDraw = new DebugDraw();
+  debugDraw.SetSprite(ctx);
   debugDraw.SetDrawScale(30.0);
   debugDraw.SetFillAlpha(0.5);
   debugDraw.SetLineThickness(1.0);
-  debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
+  debugDraw.SetFlags(DebugDraw.e_shapeBit | DebugDraw.e_jointBit);
   world.SetDebugDraw(debugDraw);
 
   window.setInterval(update, 1000 / 60);
@@ -113,8 +136,8 @@ function init() {
   }
 
   function getBodyAtMouse() {
-    mousePVec = new b2Vec2(mouseX, mouseY);
-    var aabb = new b2AABB();
+    mousePVec = new Vec2(mouseX, mouseY);
+    var aabb = new Aabb();
     aabb.lowerBound.Set(mouseX - 0.001, mouseY - 0.001);
     aabb.upperBound.Set(mouseX + 0.001, mouseY + 0.001);
 
@@ -125,7 +148,7 @@ function init() {
   }
 
   function getBodyCB(fixture) {
-    if (fixture.GetBody().GetType() != b2Body.b2_staticBody) {
+    if (fixture.GetBody().GetType() != Body.b2_staticBody) {
       if (
         fixture
           .GetShape()
@@ -143,7 +166,7 @@ function init() {
     if (isMouseDown && !mouseJoint) {
       var body = getBodyAtMouse();
       if (body) {
-        var md = new b2MouseJointDef();
+        var md = new MouseJointDef();
         md.bodyA = world.GetGroundBody();
         md.bodyB = body;
         md.target.Set(mouseX, mouseY);
@@ -156,7 +179,7 @@ function init() {
 
     if (mouseJoint) {
       if (isMouseDown) {
-        mouseJoint.SetTarget(new b2Vec2(mouseX, mouseY));
+        mouseJoint.SetTarget(new Vec2(mouseX, mouseY));
       } else {
         world.DestroyJoint(mouseJoint);
         mouseJoint = null;
@@ -193,4 +216,11 @@ function init() {
   }
 }
 
-init();
+addEventListener('load', ({ target }) => {
+  const { innerWidth: w, innerHeight: h } = window;
+
+  c.width = c.style.width = w;
+  c.height = c.style.height = h;
+
+  init();
+});
--- src/example.js
+++ src/example.js
@@ -1,4 +1,4 @@
-import { Box2D } from './box2d';
+import { Box2D } from './box2dweb';
 
 let world;
 
