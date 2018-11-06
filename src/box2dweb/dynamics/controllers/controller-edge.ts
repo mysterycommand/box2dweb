38f5c4974f4fed61e3b3f4099e64e478bf83eb94
Matt Hayes <matt@mysterycommand.com>
2018-11-06 15:13:07 -0500

    refactor(controller): rough re-implementation of the base controller list structure
    not sure about the naming and it looks like this isn't used in the demo I have so I'm leaving i.js
    untouched until I understand it better

---
+++ src/box2dweb/dynamics/controllers/controller-edge.ts
@@ -0,0 +1,8 @@
+export default class ControllerEdge {
+  public controller: any;
+  public body: any;
+  public nextBody: any;
+  public prevBody: any;
+  public nextController: any;
+  public prevController: any;
+}
